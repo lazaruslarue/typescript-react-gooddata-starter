24e85ceeb4c3d91c21238e6cdbc6d066b4a15cad
jesse <jesse@hvst.com>
2018-10-25 15:16:40 -0500

    use gd sdk to get loggedin status & log that. also don't console errors on dev

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,27 @@ import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+import { factory } from '@gooddata/gooddata-js';
+
+const domain = 'localhost:3000'
+const sdk = factory({domain})
+    // tslint:disable-next-line:no-console
+    console.log('opens')
+sdk.user.isLoggedIn().then((isLogged)=> {
+    // tslint:disable-next-line:no-console
+  console.log('sdk returned isLogged: ', isLogged);
+  if (isLogged) {
+    // tslint:disable-next-line:no-console
+    console.log('isLogged');
+    ReactDOM.render(
+      <App />,
+      document.getElementById('root') as HTMLElement
+    );
+    registerServiceWorker();
+    } else {
+    // tslint:disable-next-line:no-console
+      console.log('aintLogged');
+      window.location.replace(`${domain}/account.html?lastUrl=${encodeURIComponent(window.location.href)}`);
+    }
+  }
 );
-registerServiceWorker();
