acf82116134ac6aa6b5b75f3202a31e939d3df4a
jesse <jesse@hvst.com>
2018-10-31 14:29:02 -0500

    add analytics component

---
+++ src/Analytics/TempGoodData/PageStatistics.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Component } from 'react'
+
+import { Visualization } from '@gooddata/react-components';
+import '@gooddata/react-components/styles/css/main.css';
+
+export default class PageStatistics extends Component {
+  public render() {
+    return (
+      <div  style={{ height: 300 }} className="s-visualization-chart">
+        <Visualization
+          projectId='t3izalac0vx1rab9avgb0mjicmhlgjvh'
+          identifier='aarabodYdMhq'
+          // uri="/gdc/md/t3izalac0vx1rab9avgb0mjicmhlgjvh/obj/810"
+        />
+      </div>
+    )
+  }
+}
