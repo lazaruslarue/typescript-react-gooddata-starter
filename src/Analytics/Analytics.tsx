aa552fff1b9ead4ac0ee676e30e91380443916d6
jesse <jesse@hvst.com>
2018-11-01 13:02:19 -0500

    use new analytics nav component

--- src/Analytics/Analytics.tsx
+++ src/Analytics/Analytics.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { Component } from 'react'
 
-import { Link, Route } from 'react-router-dom';
+import {  Route } from 'react-router-dom';
 
 import './Analytics.css';
 
+import AnalyticsNav from './AnalyticsNav/AnalyticsNav';
 import PageStatistics from './TempGoodData/PageStatistics';
 import PostStatistics from './TempGoodData/PostStatistics';
 
@@ -12,12 +13,7 @@ export default class Analytics extends Component {
   public render(){
     return (
       <div>
-        <nav>
-          <ul>
-            <li><Link to="/page">Page Statistics</Link></li>
-            <li><Link to="/post">Post Statistics</Link></li>
-          </ul>
-        </nav>
+        <AnalyticsNav />
         <Route exact={true} path="/page" component={PageStatistics}/>
         <Route exact={true} path="/post" component={PostStatistics}/>
       </div>
