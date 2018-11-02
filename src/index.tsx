e672c64f25ba700a22f77a6d27b96c9dad2c6382
jesse <jesse@hvst.com>
2018-11-01 19:22:59 -0500

    Wrap with User Provider

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 import { factory } from '@gooddata/gooddata-js';
 import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './UserProvider/UserProvider';
 
 const domain = 'localhost:3000' // TODO: this needs to be ENV... set to GD proxy location on build.
 const gooddataSDK = factory({domain})
@@ -13,9 +14,11 @@ const gooddataSDK = factory({domain})
 gooddataSDK.user.isLoggedIn().then((isLogged)=> {
   if (isLogged) {
     ReactDOM.render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>,
+      <UserProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UserProvider>,
       document.getElementById('root') as HTMLElement
     );
     registerServiceWorker();
