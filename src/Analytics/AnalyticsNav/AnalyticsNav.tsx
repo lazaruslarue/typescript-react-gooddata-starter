f5ddc76ec84a55c50c2a5fa3e74e40e0d6cd6909
jesse <jesse@hvst.com>
2018-11-02 18:03:22 -0500

    rudimentary permissions visibiility funcitonality

--- src/Analytics/AnalyticsNav/AnalyticsNav.tsx
+++ src/Analytics/AnalyticsNav/AnalyticsNav.tsx
@@ -3,16 +3,23 @@ import { Component } from 'react'
 import { Link } from 'react-router-dom';
 
 import './AnalyticsNav.css';
+import User, { UserSubscribe, UserContainer } from 'src/UserProvider/UserProvider';
 
 export default class AnalyticsNav extends Component {
   public render(){
+
     return (
-    <nav className="Analytics-nav">
-      <ul>
-        <li><Link to="/page">Page Statistics</Link></li>
-        <li><Link to="/post">Post Statistics</Link></li>
-      </ul>
-    </nav>
+      <UserSubscribe to={[User]}>
+        { (user:UserContainer) => (
+        <nav className="Analytics-nav" style={{display: user.state.loggedIn && user.state.analyticsEnabled ? undefined: 'none'}}>
+          <ul>
+            <li><Link to="/page">Page Statistics</Link></li>
+            <li><Link to="/post">Post Statistics</Link></li>
+          </ul>
+        </nav>
+        )}
+      </UserSubscribe>
+
    )
   }
 }
