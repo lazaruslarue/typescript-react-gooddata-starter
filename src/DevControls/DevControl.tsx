ad3c947b596c09bfe07ee025c5742295a6753189
jesse <jesse@hvst.com>
2018-11-02 17:55:43 -0500

    use the new states and material components to change them

--- src/DevControls/DevControl.tsx
+++ src/DevControls/DevControl.tsx
@@ -1,14 +1,64 @@
 import * as React from "react";
+
+import { FormGroup, FormControlLabel } from '@material-ui/core';
+
+import Checkbox from '@material-ui/core/Checkbox'
+
+
 import User, { UserContainer, UserSubscribe } from "../UserProvider/UserProvider";
 
 const DevControls = () => {
   return (
     <UserSubscribe to={[User]}>
      { (user: UserContainer) => (
-          <div>
-            <button onClick={()=> user.login()}>Login</button>
-            <button onClick={()=> user.logout()}>Logout</button>
-          </div>
+       <FormGroup >
+         <FormControlLabel
+          control={
+            <Checkbox 
+              checked={user.state.loggedIn} 
+              onChange={()=> user.setState({loggedIn: !user.state.loggedIn})}
+            />
+          }
+          label="Logged In"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox 
+              checked={user.state.analyticsEnabled} 
+              onChange={()=> user.setState({analyticsEnabled: !user.state.analyticsEnabled})}
+            />
+          }
+          label="Analytics Enabled"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox 
+              checked={user.state.premiumAnalytics} 
+              onChange={()=> user.setState({premiumAnalytics: !user.state.premiumAnalytics})}
+            />
+          }
+          label="Premium Analytics"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox 
+              checked={user.state.showLeadgen} 
+              onChange={()=> user.setState({showLeadgen: !user.state.showLeadgen})}
+            />
+          }
+          label="Show Leadgen"
+        />       
+        <FormControlLabel
+          control={
+            <Checkbox 
+              checked={user.state.showBanners} 
+              onChange={()=> user.setState({showBanners: !user.state.showBanners})}
+            />
+          }
+          label="Show Banners"
+        />
+       </FormGroup>
+
         )
      }
     </UserSubscribe>
