d6f03fd44cb374398add4c902fe8aaadbc3b3c53
jesse <jesse@hvst.com>
2018-11-05 18:49:03 -0600

    add placeholders for Premium page/post

--- src/Analytics/TempGoodData/PostStatistics.tsx
+++ src/Analytics/TempGoodData/PostStatistics.tsx
@@ -5,14 +5,15 @@ import { Visualization } from '@gooddata/react-components';
 import '@gooddata/react-components/styles/css/main.css';
 
 export default class PostStatistics extends Component {
-    public render() {
-        return (
-            <div>
-                <Visualization
-                    projectId='t3izalac0vx1rab9avgb0mjicmhlgjvh'
-                    identifier='acjabr3ZfGHG'
-                />
-            </div>
-        )
-    }
+  public render() {
+    return (
+      <div>
+        Basic Post Stats
+        <Visualization
+          projectId='t3izalac0vx1rab9avgb0mjicmhlgjvh'
+          identifier='acjabr3ZfGHG'
+        />
+      </div>
+    )
+  }
 }
