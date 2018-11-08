ca1b20a3e286adc2d561a9548e0b5bab043cddce
jesse <jesse@hvst.com>
2018-11-08 17:35:17 -0600

    BannerProvider needs rename to BannerReportXYZ

---
+++ src/BannerProvider/BannerProvider.tsx
@@ -0,0 +1,45 @@
+
+import * as React from "react";
+import { Container, Provider, ProviderProps, Subscribe, SubscribeProps } from "unstated";
+
+interface IBannerState {
+  banners: []
+}
+
+export class BannerContainer extends Container<IBannerState> {
+  constructor() {
+    super();
+    this.state = {
+      banners: []
+    }
+    this.getBanner()
+  }
+
+  // get a banner 
+  private async getBanner() {
+    return fetch("/api/get_me_a_banner")
+
+      .then(json => {
+
+        console.log(json)
+
+      })
+      .catch(reason => {
+        console.log(reason)
+      })
+  });
+}
+
+}
+
+const Banner = new BannerContainer();
+
+export const BannerProvider = (props: ProviderProps) => {
+  return <Provider inject={props.inject || [Banner]}>{props.children}</Provider>
+}
+
+export const BannerSubscribe = (props: SubscribeProps) => {
+  return <Subscribe to={props.to || [Banner]}>{props.children}</Subscribe>
+}
+
+export default Banner;
