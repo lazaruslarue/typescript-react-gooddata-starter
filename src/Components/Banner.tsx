a38a2f8270bf8efbc6470d2931bb02ccce8dc78d
jesse <jesse@hvst.com>
2018-11-12 15:46:52 -0600

    make email banners great again

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -5,7 +5,7 @@ import './Banner.css';
 import { Button } from '@material-ui/core';
 
 export interface IBannerProps {
-  bannerHash: string | number
+  bannerHash: string
 }
 
 export interface IBannerState {
@@ -22,7 +22,6 @@ export interface IBannerState {
 
 export default class Banner extends Component<IBannerProps, IBannerState> {
 
-
   constructor(props: IBannerProps) {
     super(props);
     this.state = {
@@ -38,16 +37,16 @@ export default class Banner extends Component<IBannerProps, IBannerState> {
   /**
    * fetchBannerByHash from DB
    */
-  public async fetchBannerByHash(bannerHash: string | number): Promise<IBannerState> {
+  public async fetchBannerByHash(bannerHash: string): Promise<IBannerState> {
     return fetch(`/api/bannerhash/${bannerHash}`)
       .then(resp => resp.json())
       .then(data => data.banner)
       .catch(reason => { throw new Error(reason) })
   }
 
-  public render() {
+  public renderVerticalBanner() {
     return (
-      <div className="bannercontainer">
+      <div className="bannercontainer vertical">
         {this.state.sponsored_by ? (<span className={"banner-sponsor"}>{this.state.sponsored_by}</span>) : null}
         {this.state.title ? (<span className={"banner-title"}> {this.state.title} </span>) : null}
         {this.state.img_url ? <img src={this.state.img_url} /> : ''}
@@ -60,5 +59,15 @@ export default class Banner extends Component<IBannerProps, IBannerState> {
           <Button variant="contained" style={{ background: this.state.button_color || "#2196f3" }}>{this.state.button_text}</Button>) : null}
       </div>
     )
+  }
+  public renderEmailBanner() {
+    return (
+      <div className="bannercontainer email">
+        <img src={this.state.img_url} />
+      </div>
+    )
+  }
+  public render() {
+    return this.state.email_banner ? this.renderEmailBanner() : this.renderVerticalBanner()
   }
 }
